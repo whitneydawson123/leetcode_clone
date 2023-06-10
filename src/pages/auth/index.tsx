b1b4a503c797ebbc07adf5c348e7d331779bd1c9
Whitney <whitneydawson123@gmail.com>
2023-06-10 04:10:38 -0400

    finished navbar

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '@/src/components/Navbar/Navbar';
+
+type AuthPageProps = {};
+
+const AuthPage:React.FC<AuthPageProps> = () => {
+  
+  return <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
+    <div className='max-w-7xl mx-auto'>
+      <Navbar />
+      <div className='flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none'>
+        <img src='./hero.png' alt='hero image' />
+      </div>
+    </div>
+  </div>
+}
+export default AuthPage;
